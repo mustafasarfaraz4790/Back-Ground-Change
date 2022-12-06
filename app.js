437d48561ce5edea6fc1368f3c288342ae9dabaf
Mustafa <114693151+mustafasarfaraz4790@users.noreply.github.com>
2022-12-06 21:05:52 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,30 @@
+function red() {
+  document.bgColor = "red";
+}
+function green() {
+  document.bgColor = "green";
+}
+function purple() {
+  document.bgColor = "purple";
+}
+function pink() {
+  document.bgColor = "pink";
+}
+function yellow() {
+  document.bgColor = "yellow";
+}
+function blue() {
+  document.bgColor = "blue";
+}
+function gold() {
+  document.bgColor = "gold";
+}
+function black() {
+  document.bgColor = "black";
+}
+function orange() {
+  document.bgColor = "orange";
+}
+function brown() {
+  document.bgColor = "brown";
+}
